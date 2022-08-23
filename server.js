4c0a8f296de54b2495b7cad2b2a79f4b0ea1c36c
Sam Karp <sfcypret@gmail.com>
2022-08-22 22:15:09 -0400

    express and mysql connected and tested

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3002;
 const app = express();
@@ -5,12 +6,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost', 
+        //Your MySql username,
+        user: 'root', 
+        // Your MySql password
+        password: '', 
+        database: 'election'
+    }, 
+    console.log('Connected to the election database.')
+);
 
 
 
 
-
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 //Default response for any other request(Not Found)
