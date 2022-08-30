6b184c764b7b44755b0b9ed8dc6db2bbbe1a00ea
Sam Karp <sfcypret@gmail.com>
2022-08-29 22:22:02 -0400

    updated endpoints

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/api/candidates', (req, res) => {
 // GET a single candidate
 //api endpoint includes value of id to specify which candidate selecting from db
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT candidates.* parties.name
+    const sql = `SELECT candidates.*, parties.name
     AS party_name
     FROM candidates
     LEFT JOIN parties
@@ -66,6 +66,66 @@ db.query(sql, params, (err, row) => {
         data: row
     });
 });
+});
+
+//Create a candidate
+//uses the http request method post() using endpoint /api/candidate
+                            //using object destructuring to pull body property out
+                            app.post('/api/candidate', ({ body }, res) => {
+                                //assign errors to receive the return from inputCheck function
+                                const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+                                //validate user data before the changes are inserted into the database
+                                if (errors) {
+                                    res.status(400).json({ error: errors });
+                                    return;
+                                }
+                            
+                            
+                                const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                                VALUES (?,?,?)`;
+                                const params = [body.first_name, body.last_name, body.industry_connected];
+                            
+                                db.query(sql, params, (err, result) => {
+                                    if (err) {
+                                        res.status(400).json({ error: err.message });
+                                        return;
+                                    }
+                                    res.json({
+                                        message: 'success', 
+                                        data: body
+                                    });
+                                });
+                            });
+
+//Update a candidate's party
+//forces any PUT request to /api/candidate/:id to include a party_id property
+//even if the intention is to remove a party affiliatio by setting it to null, party_id is required
+app.put('/api/candidate/:id', (req, res) =>{
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `UPDATE candidates SET party_id = ?
+    WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            //check if a record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'success', 
+                data: req.body, 
+                changes: result.affectedRows
+            });
+        }
+    });
 });
 
 
@@ -94,32 +154,57 @@ db.query(sql, params, (err, result) => {
 });
 });
 
-//Create a candidate
-//uses the http request method post() using endpoint /api/candidate
-                            //using object destructuring to pull body property out
-app.post('/api/candidate', ({ body }, res) => {
-    //assign errors to receive the return from inputCheck function
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    //validate user data before the changes are inserted into the database
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
+//route to get all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success', 
+            data: rows
+        });
+    });
+});
 
-
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-    VALUES (?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-
-    db.query(sql, params, (err, result) => {
+//route that includes id parameter for single party
+app.get('/api/party/:id', (req, res) =>{
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
         if (err) {
             res.status(400).json({ error: err.message });
             return;
         }
         res.json({
             message: 'success', 
-            data: body
+            data: row 
         });
+    });
+});
+
+
+
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            //checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted', 
+                changes: result.affectedRows, 
+                id: req.params.id 
+            });
+        }
     });
 });
 
