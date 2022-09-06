95b7c8b168475185097594d6842840e23c055f32
Sam Karp <sfcypret@gmail.com>
2022-09-06 19:51:45 -0400

    add voter table

--- server.js
+++ server.js
@@ -214,7 +214,11 @@ app.use((req, res) => {
     res.status(404).end();
 });
 
-
+//start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
 });
